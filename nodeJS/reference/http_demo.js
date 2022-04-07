9c61b8bc43c018d7dd33b5a1305296f1fcfddcba
Josh Mitchell <jmitch13@csu.fullerton.edu>
2022-04-07 08:20:18 -0700

    This is removing all of the nodeJS files

--- nodeJS/reference/http_demo.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const { runInNewContext } = require('vm');
-
-//create a server object
-http.createServer((req, res) => {
-    //Write a response
-    res.write('Hello World');
-    res.end();
-}).listen(5000, () => console.log('Server running...'));
--- nodeJS/tutorial.js
+++
@@ -1,9 +0,0 @@
-const Logger = require('./logger.js');
-
-console.log(Logger);
-
-const logger = new Logger.Logger();
-
-logger.on('message', (data) => console.log('Called Listener', data));
-
-logger.log('Hello World');
